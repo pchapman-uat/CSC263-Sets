212fb873c0d28719b41134f949f53fd99bc56fea
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 11:40:43 -0700

    Add and print HashSet

--- Main.java
+++ Main.java
@@ -3,6 +3,19 @@ import java.util.HashSet;
 public class Main {
     public static HashSet<Integer> set = new HashSet<Integer>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       randomHashSet(5);
+       printHashSet();
+    }
+
+    private static void randomHashSet(int len){
+        for (int i = 0; i < len; i++) {
+            set.add((int)(Math.random()*100));
+        }
+    }
+
+    private static void printHashSet(){
+        for (Integer i:set) {
+            System.out.println(i);
+        }
     }
 }
--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # Expectation
 [X]Create a HashSet of integers.
-[ ]Add elements to the set.
+[X]Add elements to the set.
 [ ]Remove elements from the set.
 [ ]Check if an element exists in the set.
-[ ]Print all elements in the set.
+[X]Print all elements in the set.
 [ ]Unique Elements: Write a method 'uniqueElements(int[] array)' that takes an array of integers and returns a Set containing only the unique elements from the array.
