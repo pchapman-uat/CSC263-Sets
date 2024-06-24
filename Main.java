80881f7f1122956b27d8007a1d6fd542f740b70b
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 11:57:51 -0700

    Unique Elements

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static HashSet<Integer> set = new HashSet<Integer>();
     public static void main(String[] args) {
-        randomHashSet(5);
+        randomHashSet(10);
         printHashSet();
         System.out.println(isInHashSet(5));
         if(removeHashSet(4)) System.out.println("Number 4 was removed");
         printHashSet();
+        System.out.println(uniqueElements(2,4,1,2,3,4,5,1,4,6));
     }
 
+    private static <T> Set<T> uniqueElements(T... values){
+        Set<T> set = new HashSet<T>();
+        for (T t : values) {
+            set.add(t);
+        }
+        return set;
+    }
     private static boolean removeHashSet(int num){
         return set.remove(num);
     }
--- TODO.md
+++ TODO.md
@@ -4,4 +4,4 @@
 [X]Remove elements from the set.
 [X]Check if an element exists in the set.
 [X]Print all elements in the set.
-[ ]Unique Elements: Write a method 'uniqueElements(int[] array)' that takes an array of integers and returns a Set containing only the unique elements from the array.
+[X]Unique Elements: Write a method 'uniqueElements(int[] array)' that takes an array of integers and returns a Set containing only the unique elements from the array.
