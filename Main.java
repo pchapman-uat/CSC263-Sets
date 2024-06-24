63cc0bab5548025e34661f9a89626c60ec6ca7d1
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 12:04:21 -0700

    User in hash set

--- Main.java
+++ Main.java
@@ -4,23 +4,31 @@ import java.util.Set;
 
 public class Main {
     public static HashSet<Integer> set = new HashSet<Integer>();
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         userGenerateSet();
+        System.out.println("-".repeat(20));
         printHashSet();
-        System.out.println(isInHashSet(5));
+        System.out.println("-".repeat(20));
+        userInHashSet();
+        System.out.println("-".repeat(20));
         if(removeHashSet(4)) System.out.println("Number 4 was removed");
         printHashSet();
         System.out.println(uniqueElements(2,4,1,2,3,4,5,1,4,6));
     }
 
+    private static void userInHashSet(){
+        System.out.println("What number do you want to look for?");
+        int num = scanner.nextInt();
+        if(isInHashSet(num)) System.out.println("Number " + num + " is in the set");
+        else System.out.println("Number " + num + " is not in the set");
+    }
     private static void userGenerateSet(){
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of elements in the set:");
         int len = scanner.nextInt();
         System.out.println("Enter the maximum value of the set:");
         int max = scanner.nextInt();
         randomHashSet(len, max);
-        scanner.close();
     }
 
     private static <T> Set<T> uniqueElements(T... values){
