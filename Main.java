90acbb196b1f5976eecb86e95d98e4a21496ca61
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 12:01:09 -0700

    User Generate Set

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
     public static HashSet<Integer> set = new HashSet<Integer>();
     public static void main(String[] args) {
-        randomHashSet(10);
+        userGenerateSet();
         printHashSet();
         System.out.println(isInHashSet(5));
         if(removeHashSet(4)) System.out.println("Number 4 was removed");
         printHashSet();
         System.out.println(uniqueElements(2,4,1,2,3,4,5,1,4,6));
+    }
+
+    private static void userGenerateSet(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the set:");
+        int len = scanner.nextInt();
+        System.out.println("Enter the maximum value of the set:");
+        int max = scanner.nextInt();
+        randomHashSet(len, max);
+        scanner.close();
     }
 
     private static <T> Set<T> uniqueElements(T... values){
@@ -29,8 +40,11 @@ public class Main {
 
 
     private static void randomHashSet(int len){
+        randomHashSet(len, 10);
+    }
+    private static void randomHashSet(int len, int max){
         for (int i = 0; i < len; i++) {
-            set.add((int)(Math.random()*10));
+            set.add((int)(Math.random()*max));
         }
     }
 
