ff3f61f6dc5581911e017e8da18cb60a68b2b95e
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 11:41:50 -0700

    Check if in set

--- Main.java
+++ Main.java
@@ -3,13 +3,19 @@ import java.util.HashSet;
 public class Main {
     public static HashSet<Integer> set = new HashSet<Integer>();
     public static void main(String[] args) {
-       randomHashSet(5);
-       printHashSet();
+        randomHashSet(5);
+        printHashSet();
+        System.out.println(isInHashSet(5));
     }
+
+    private static boolean isInHashSet(int num){
+        return set.contains(num);
+    }
+
 
     private static void randomHashSet(int len){
         for (int i = 0; i < len; i++) {
-            set.add((int)(Math.random()*100));
+            set.add((int)(Math.random()*10));
         }
     }
 
--- TODO.md
+++ TODO.md
@@ -2,6 +2,6 @@
 [X]Create a HashSet of integers.
 [X]Add elements to the set.
 [ ]Remove elements from the set.
-[ ]Check if an element exists in the set.
+[X]Check if an element exists in the set.
 [X]Print all elements in the set.
 [ ]Unique Elements: Write a method 'uniqueElements(int[] array)' that takes an array of integers and returns a Set containing only the unique elements from the array.
